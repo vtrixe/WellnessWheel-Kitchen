5f7992b264d740c4c8c4b9a76e738f85ee59e113
VIKRANT SHARMA <vikrant500sharma@gmail.com>
2024-01-29 02:27:59 +0530

    domains

--- packages/authentication/src/app/domain/domain.module.ts
+++ packages/authentication/src/app/domain/domain.module.ts
@@ -1,10 +1,25 @@
 /* eslint-disable prettier/prettier */
 import { Module } from "@nestjs/common";
 import { AuthModule } from "./auth/auth.module";
-import { CoreModule } from "./core/core.module";
+import { TerminusModule } from "@nestjs/terminus";
+import { ConfigModule } from "src/config/config.module";
+import { AppLoggerModule } from "src/logger/logger.module";
+import { DBModule } from "src/storage/database/db.module";
+
+import { UserEntity } from "./user/entity/user.entity";
+import { UserModule } from "./user/user.module";
 
 @Module({
-  imports: [AuthModule, CoreModule],
+  imports: [
+    DBModule.forRoot({
+      entities: [UserEntity],
+    }),
+    UserModule,
+    AuthModule,
+    TerminusModule,
+    AppLoggerModule,
+    ConfigModule,
+  ],
   controllers: [],
   providers: [],
 })
--- packages/authentication/src/config/config.interface.ts
+++ packages/authentication/src/config/config.interface.ts
@@ -1,12 +1,27 @@
-
 export interface ConfigDatabase {
-    url : string;
-}
-
-
-
-export interface ConfigData {
-    env: StorageManager;
+    url: string;
+  }
+  
+  export interface ConfigSwagger {
+    username: string;
+    password: string;
+  }
+  
+  export interface AuthConfig {
+    expiresIn: number;
+    secret: string;
+  }
+  
+  export interface ConfigData {
+    env: string;
+  
     port: number;
-    db: ConfigDatabase
-}
+  
+    db: ConfigDatabase;
+  
+    swagger: ConfigSwagger;
+  
+    logLevel: string;
+  
+    auth: AuthConfig;
+  }
